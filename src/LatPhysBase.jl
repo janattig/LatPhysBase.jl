8dcad7f5ace4bee3545158bc33d19f8ffd086d82
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-15 11:11:56 +0100

    imported base definitions

--- src/LatPhysBase.jl
+++ src/LatPhysBase.jl
@@ -1,5 +1,78 @@
+################################################################################
+#
+#   module LatticePhysics_Base
+#
+#   --> ABSTRACT TYPE DEFINITION
+#           - AbstractSite{L,D}
+#           - AbstractBond{L,N}
+#           - AbstractUnitcell{S,B}
+#           - AbstractLattice{S,B,U}
+#
+#   --> INTERFACING ABSTRACT TYPES
+#           - AbstractSite{L,D}
+#               - positions
+#               - label
+#           - AbstractBond{L,N}
+#               - connecting identifiers
+#               - label
+#               - wrap
+#           - AbstractUnitcell{S,B}
+#               - Bravais lattice vectors
+#               - sites
+#               - bonds
+#           - AbstractLattice{S,B,U}
+#               - Unitcell
+#               - Bravais lattice vectors
+#               - sites
+#               - bonds
+#
+#   --> NAIVE STRUCT DEFINITIONS
+#           - Site{L,D}
+#           - Bond{L,N}
+#           - Unitcell{S,B}
+#           - Lattice{S,B,U}
+#
+#   --> IMPLEMENTING JULIA.BASE FUNCTIONS
+#           - show
+#           - print
+#
+################################################################################
+
+# module start
 module LatPhysBase
 
-greet() = print("Hello World!")
 
-end # module
+
+
+# ABSTRACT TYPES INCLUDING INTERFACE DEFINITION
+
+# Sites
+include("abstract_site.jl")
+# Bonds
+include("abstract_bond.jl")
+# Unitcells
+include("abstract_unitcell.jl")
+# Lattices
+include("abstract_lattice.jl")
+
+
+
+# CONCRETE TYPES INCLUDING INTERFACE IMPLEMENTATION
+
+# Sites
+include("concrete_site.jl")
+# Bonds
+include("concrete_bond.jl")
+# Unitcells
+include("concrete_unitcell.jl")
+# Lattices
+include("concrete_lattice.jl")
+
+
+
+# CUSTOM NICE PRINTING
+include("show.jl")
+
+
+# module end
+end
