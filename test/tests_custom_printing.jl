8437930a809af02fdc8294df84b2bdc45f054fb9
Jan <jattig@thp.uni-koeln.de>
2018-11-23 14:59:31 +0100

    added more tests (bonds)

--- test/tests_custom_printing.jl
+++ test/tests_custom_printing.jl
@@ -10,13 +10,29 @@
         println("---")
         @test_nowarn println(newSite(Site{String,2}, [0.0, 0.0], "site 1"))
         @test_nowarn println(newSite(Site{Int64,2}, [0.0, 0.0], 1))
-        @test_nowarn println(newSite(Site{Float64,2}, [0.0, 0.0], 1.0))
+        @test_nowarn println(newSite(Site{Float64,3}, [0.0, 0.0, 0.0], 1.0))
 
         # multiple sites (list of sites)
         println("---")
         @test_nowarn println([newSite(Site{Int64,2}, [0.0, 0.0], i) for i in 1:10])
-        println("---")
 
+    end
+
+
+    #-----------------
+    #   BONDS
+    #-----------------
+    @testset "Bonds" begin
+
+        # single bond
+        println("---")
+        @test_nowarn println(newBond(Bond{String,2}, 1,1, "site 1", (0,0)))
+        @test_nowarn println(newBond(Bond{Int64,3}, 1,1, 1, (0,0,1)))
+        @test_nowarn println(newBond(Bond{Float64,2}, 1,1, 1.0, (0,0)))
+
+        # multiple sites (list of bonds)
+        println("---")
+        @test_nowarn println([newBond(Bond{Int64,2}, 1,1, i, (0,0)) for i in 1:10])
 
     end
 
