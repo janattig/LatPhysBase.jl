c3acd5e84bba96bdcab33e20bc346ddd6e9f7ae4
Jan <jattig@thp.uni-koeln.de>
2018-11-23 14:56:21 +0100

    added more tests

--- test/tests_custom_printing.jl
+++ test/tests_custom_printing.jl
@@ -7,7 +7,15 @@
     @testset "Sites" begin
 
         # single site
+        println("---")
         @test_nowarn println(newSite(Site{String,2}, [0.0, 0.0], "site 1"))
+        @test_nowarn println(newSite(Site{Int64,2}, [0.0, 0.0], 1))
+        @test_nowarn println(newSite(Site{Float64,2}, [0.0, 0.0], 1.0))
+
+        # multiple sites (list of sites)
+        println("---")
+        @test_nowarn println([newSite(Site{Int64,2}, [0.0, 0.0], i) for i in 1:10])
+        println("---")
 
 
     end
