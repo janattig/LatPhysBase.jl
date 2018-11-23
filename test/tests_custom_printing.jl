6dbacfa7cbb9304e1bc654dbe19b0ba8687d8013
Jan <jattig@thp.uni-koeln.de>
2018-11-23 14:48:36 +0100

    added a single test for pretty printing

--- test/tests_custom_printing.jl
+++ test/tests_custom_printing.jl
@@ -1,7 +1,16 @@
 # TESTSET FOR ALL CUSTOM PRINTING FUNCTIONS
 @testset "Custom Pretty Printing of Types" begin
 
+    #-----------------
+    #   SITES
+    #-----------------
+    @testset "Sites" begin
 
+        # single site
+        @test_nowarn println(newSite(Site{String,2}, [0.0, 0.0], "site 1"))
+
+
+    end
 
 # end the testset
 end
