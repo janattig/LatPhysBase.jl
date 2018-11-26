dbcfd6900207bfa1bd838bcce0d129fc22ed516f
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-26 15:39:27 +0100

    added specification how to show various types of bond and site labels

--- src/show.jl
+++ src/show.jl
@@ -4,15 +4,41 @@
 
 # ABSTRACT TYPES
 
+
+
 # single SITE
+
+# all labels
 function show(io::IO, s::S) where {L,D,S<:AbstractSite{L,D}}
     print(io, S, " @", point(s), ": ", label(s))
 end
+# string labels
+function show(io::IO, s::S) where {L<:AbstractString,D,S<:AbstractSite{L,D}}
+    print(io, S, " @", point(s), ": \"", label(s),"\"")
+end
+# symbol labels
+function show(io::IO, s::S) where {L<:Symbol,D,S<:AbstractSite{L,D}}
+    print(io, S, " @", point(s), ": :", label(s))
+end
+
+
 
 # single BOND
+
+# all labels
 function show(io::IO, b::B) where {L,N,B<:AbstractBond{L,N}}
     print(io, B, " ", from(b), "-->", to(b), " @", wrap(b), ": ", label(b))
 end
+# string labels
+function show(io::IO, b::B) where {L<:AbstractString,N,B<:AbstractBond{L,N}}
+    print(io, B, " ", from(b), "-->", to(b), " @", wrap(b), ": \"", label(b),"\"")
+end
+# symbol labels
+function show(io::IO, b::B) where {L<:Symbol,N,B<:AbstractBond{L,N}}
+    print(io, B, " ", from(b), "-->", to(b), " @", wrap(b), ": :", label(b))
+end
+
+
 
 # single UNITCELL
 function show(io::IO, u::U) where {D,N,LS,LB,S<:AbstractSite{LS,D},B<:AbstractBond{LB,N},U<:AbstractUnitcell{S,B}}
